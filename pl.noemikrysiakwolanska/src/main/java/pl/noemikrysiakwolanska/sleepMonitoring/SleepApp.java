e79efa7e65aa7fb55860724cead08d03e3b43937
Noemi Krysiak-Wolanska <noemi.krysiak@gmail.com>
2021-03-06 19:49:21 +0100

    Added SleepApp class

---
+++ pl.noemikrysiakwolanska/src/main/java/pl/noemikrysiakwolanska/sleepMonitoring/SleepApp.java
@@ -0,0 +1,12 @@
+package pl.noemikrysiakwolanska.sleepMonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepApp{
+    public static void main(String[] args) {
+        SpringApplication.run(SleepApp.class, args);
+    }
+
+}
